1358a495167366f08c18cd1901c1236ac1319755
Michael Goodwin <mikejg101@icloud.com>
2023-05-29 18:46:03 -0400

    chore: migrated license check to typescript

--- tools/license-check.ts
+++ tools/license-check.ts
@@ -9,8 +9,8 @@ import fs from 'fs';
 // Get a reference to the console logger so we can turn it back on if needed.
 const log = console.log;
 
-const pathOfPackageJson = path.join(process.cwd(), 'package.json');
-const pathOfModules = path.join(process.cwd(), 'node_modules');
+const packageJsonPath = path.join(process.cwd(), 'package.json');
+const nodeModulesPath = path.join(process.cwd(), 'node_modules');
 const regex = /incompatible/g;
 const errorTitle = 'License Issues Found In Production Packages.';
 const successTitle = 'No License Issues Found In Production Packages.';
@@ -23,10 +23,10 @@ const colors = {
 
 /**
  * Returns the header to be displayed in the console.
- * @param hasError wether the header to include the standard title or the error title.
- * @returns the header string.
+ * @param hasError Whether the header should include the standard title or the error title.
+ * @returns The header string.
  */
-const getHeader = (hasError = false) => {
+const getHeader = (hasError = false): string => {
   const color = getStatusColor(hasError ? 'error' : 'success');
   const paddedTitle = (hasError ? errorTitle : successTitle).padEnd(66, ' ');
   const paddedBorder = `${color}`.padEnd(74, '*') + '\x1B[39m';
@@ -36,21 +36,21 @@ const getHeader = (hasError = false) => {
 /**
  * Returns the status color from the color table. Returns the default
  * color if the specified status is not found.
- * @param status the status from the colors table.
- * @returns the status color.
+ * @param status The status from the colors table.
+ * @returns The status color.
  */
-const getStatusColor = (status: keyof typeof colors) => {
+const getStatusColor = (status: keyof typeof colors): string => {
   const color = colors[status];
   return color ? color : colors.default;
 };
 
 /**
  * Get the licenses for the packages passed into the function.
- * @param packages The packages to check
- * @param licenses The licenses to search
- * @returns The list of packages licenses
+ * @param packages The packages to check.
+ * @param licenses The licenses to search.
+ * @returns The list of package licenses.
  */
-const getPackageLicenses = (packages: string[], licenses: string[]) => {
+const getPackageLicenses = (packages: string[], licenses: string[]): string => {
   return packages
     .map((packageName: string) =>
       licenses.find((line: string) => line.match(packageName))
@@ -60,57 +60,55 @@ const getPackageLicenses = (packages: string[], licenses: string[]) => {
 };
 
 /**
- * Get the production dependencies from the package.json
- * @param path the path to the package.json file.
- * @returns the production dependencies.
+ * Get the production dependencies from the package.json.
+ * @param packageJsonPath The path to the package.json file.
+ * @returns The production dependencies.
  */
-const getProductionDependencies = (path: string) => {
+const getProductionDependencies = (packageJsonPath: string): string[] => {
   try {
     // Read the package.json file.
-    const packageContents = fs.readFileSync(path, {
+    const packageContents = fs.readFileSync(packageJsonPath, {
       encoding: 'utf-8',
     });
 
-    // Parse it into json.
+    // Parse it into JSON.
     const packageJson = JSON.parse(packageContents);
 
     // Return the keys since they are the package names.
     return Object.keys(packageJson?.dependencies || []);
-  } catch {
+  } catch (error) {
+    console.error(`Error reading package.json: ${error}`);
     return [];
   }
 };
 
 /**
- * Returns the licenses with compatibility info
- * @returns The licenses with compatibility info
+ * Returns the licenses with compatibility info.
+ * @returns The licenses with compatibility info.
  */
-const getLicensesWithCompatibilityInfo = () =>
-  new Promise(
-    (
-      resolve: (licenseWithCompatibilityInfoLines: string[]) => void,
-      reject
-    ) => {
-      lcc.check(
-        pathOfPackageJson,
-        pathOfModules,
-        (err: Error, passed: boolean, output: string) => {
-          if (err) {
-            reject(err);
-          } else {
-            resolve(output.split('\n'));
-          }
+const getLicensesWithCompatibilityInfo = (): Promise<string[]> => {
+  return new Promise((resolve, reject) => {
+    lcc.check(
+      packageJsonPath,
+      nodeModulesPath,
+      (err: Error, passed: boolean, output: string) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(output.split('\n'));
         }
-      );
-    }
-  );
+      }
+    );
+  });
+};
 
 (async () => {
   try {
-    // Prevent the license checker logging issues with no license.
+    // Prevent the license checker from logging issues with no license.
     console.log = () => {
       return;
     };
+
     const licenseWithCompatibilityInfoLines =
       await getLicensesWithCompatibilityInfo();
 
@@ -119,7 +117,7 @@ const getLicensesWithCompatibilityInfo = () =>
 
     // Read the package.json file.
     const productionDependencyNames =
-      getProductionDependencies(pathOfPackageJson);
+      getProductionDependencies(packageJsonPath);
 
     const incompatibleProductionPackages = intersect(
       productionDependencyNames,
@@ -144,13 +142,12 @@ const getLicensesWithCompatibilityInfo = () =>
 
     console.log(results + '\n');
 
-    // Throw an error for CI builds to fail if there are license
-    // issues.
+    // Throw an error for CI builds to fail if there are license issues.
     if (hasIncompatibleLicenses) {
       process.exit(1);
     }
-  } catch {
-    console.log('Unable to check licenses.');
+  } catch (error) {
+    console.error(`Unable to check licenses: ${error}`);
     process.exit(1);
   }
 })();
