f314310e25c28b90240d517c87517bcf0a60b4e8
Michael Goodwin <mikejg101@icloud.com>
2025-04-13 12:22:58 -0400

    build: enable size tracking with size-limit

---
+++ .size-limit.js
@@ -0,0 +1,18 @@
+module.exports = [
+  {
+    path: 'dist/index.js',
+    limit: '5 KB',
+  },
+  {
+    path: 'dist/index.mjs',
+    limit: '5 KB',
+  },
+  {
+    path: 'dist/index.d.mts',
+    limit: '25 B',
+  },
+  {
+    path: 'dist/index.d.ts',
+    limit: '25 B',
+  },
+];
